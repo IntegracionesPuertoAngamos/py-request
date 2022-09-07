945c644b3312721ac2221936ee72fe5042248d30
RayzerDP <108098119+RayzerDP@users.noreply.github.com>
2022-09-07 13:51:41 -0300

    Update py-request.py

--- py-request.py
+++ py-request.py
@@ -1,21 +1,22 @@
 import requests, logging
 from datetime import datetime
-
+#Declara un tipo de formato a la fecha 
 def get_time_instance():
   now = datetime.now()
   format = "%Y-%m-%d %H:%M:%S"
   return now.strftime(format)
-
+# Se declara la url de "aut" y "usuario"
 url = "https://navisq.puertoangamos.cl/APIWsPortAngTatc/Auth/authenticate"
-# Las credenciales se encuentran en la Guía de Integración
 obj = {'username': 'username', 'password': 'password'}
 
+#Envia la de peticion de "Post"
 response = requests.post(url, json = obj)
+#Obtiene el token
 token = response.json()['token']
-
+#Se declara el metodo de "EnviarTatc" y se da un objeto vacio
 url = "https://navisq.puertoangamos.cl/APIWsPortAngTatc/PortAngTatc/EnviarTatc"
 obj = {}
-
+#Envia el objeto como json
 obj = {
   'dcNumeroTatc':             '100001127',
   'dcOperadorTatc':           'C20',
@@ -41,12 +42,13 @@ obj = {
   'dgEmisorBl':               'Navis',
   'dfLiberacion':             '2022-02-01 08:00:00'
 }
-
+#Manda la Autorizacion con el metodo post e imprime el response
 response = requests.post(url, json = obj, headers = {"Authorization": "Bearer " + token})
 print(response.text)
-
+#genera archivo de loggin y establece el formato 
 logging.basicConfig(filename = 'logs/logTATC-{}.log'.format(get_time_instance()), level = logging.INFO)
 
+#Muestra un estado de acuerdo al "response"
 if response.status_code == 400 or response.text.__contains__("E"):
   logging.error(response.text)
 else:
